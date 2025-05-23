4ae491102274f8cfca6508233ddff29b07e5e50c
HAMZA EL ALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2025-05-23 17:47:04 +0100

    Update video_creator.py

--- video_creator.py
+++ video_creator.py
@@ -4,6 +4,7 @@ from PIL import Image
 from moviepy.editor import ImageClip, AudioFileClip, concatenate_videoclips
 
 
+
 from moviepy.audio.AudioClip import CompositeAudioClip
 
 def image_audio_to_video(image_dir, audio_dir, output_path, add_voiceover, add_music, frame_durations=None):
